40a5ac2bba7baa588cad149ce96c15e51ff6eba9
Franco <riddid117@hotmail.com>
2025-06-28 16:31:16 -0300

    First commit

---
+++ src/test/java/ar/com/francoblanco/challenge/ChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.francoblanco.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
